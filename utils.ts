d3164cc55bd4e449349317805cd7fc756dd9a58e
Olivier Lemoine <lemoineolivier35@gmail.com>
2018-11-04 12:14:38 +0100

    gpujs

---
+++ utils.ts
@@ -0,0 +1,114 @@
+export class Vect3 {
+    x: number;
+    y: number;
+    z: number;
+
+    constructor(x?: number | number[], y?: number, z?: number) {
+        if (typeof x === 'number' || typeof x === 'undefined') {
+            this.x = x || 0;
+            this.y = y || 0;
+            this.z = z || 0;
+        }
+        else
+        {
+            this.x = x[0];
+            this.y = x[1];
+            this.z = x[2];
+        }
+    }
+
+    toArray(): number[] {
+        return [this.x, this.y, this.z];
+    }
+
+    apply(vect3: Vect3) {
+        this.x = vect3.x;
+        this.y = vect3.y;
+        this.z = vect3.z;
+    }
+
+    add(vect3: Vect3): Vect3 {
+        let res: Vect3 = new Vect3(
+            this.x + vect3.x,
+            this.y + vect3.y,
+            this.z + vect3.z
+        );
+        return res;
+    }
+
+    sub(vect3: Vect3): Vect3 {
+        let res: Vect3 = new Vect3(
+            this.x - vect3.x,
+            this.y - vect3.y,
+            this.z - vect3.z
+        );
+        return res;
+    }
+
+    dot(vect3: Vect3): Vect3 {
+        let res: Vect3 = new Vect3(
+            this.x * vect3.x,
+            this.y * vect3.y,
+            this.z * vect3.z
+        );
+        return res;
+    }
+
+    divide(vect3: Vect3 | number): Vect3 {
+        vect3 =
+            typeof vect3 === 'number' ? new Vect3(vect3, vect3, vect3) : vect3;
+        let res: Vect3 = new Vect3(
+            this.x / vect3.x,
+            this.y / vect3.y,
+            this.z / vect3.z
+        );
+        return res;
+    }
+
+    magnitude(): number {
+        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+    }
+
+    normalize(): Vect3 {
+        return this.divide(this.magnitude());
+    }
+
+    rotate(axis: Axis, angle: number): Vect3 {
+        let res = new Vect3(this.x, this.y, this.z);
+        switch (axis) {
+            case Axis.x:
+                res.y += res.y * Math.cos(angle);
+                res.z += res.z * Math.sin(angle);
+                break;
+            case Axis.y:
+                res.x += res.x * Math.sin(angle);
+                res.z += res.z * Math.cos(angle);
+                break;
+            default:
+                res.x += res.x * Math.cos(angle);
+                res.y += res.y * Math.sin(angle);
+                break;
+        }
+        return res;
+    }
+
+    rotateDeg(axis: Axis, angle: number): Vect3 {
+        return this.rotate(axis, (angle / Math.PI) * 180);
+    }
+
+    floor(): Vect3 {
+        return new Vect3(
+            Math.floor(this.x),
+            Math.floor(this.y),
+            Math.floor(this.z)
+        );
+    }
+
+    // rotateBase(vect3: Vect3): Vect3 {}
+}
+
+export enum Axis {
+    x = 0,
+    y = 1,
+    z = 2
+}
